835633e40875706ba50dfaa6bd34218d2c197272
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-15 22:30:26 +0000

    ready for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,70 @@
-import { TextareaAutosize } from "@mui/core";
-import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
 import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { marked } from "marked";
-import { ChangeEvent, useEffect, useState } from "react";
-import defaultContent from "./dummy";
-import Prism from "prismjs";
+
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: "Q",
+    id: "Heater-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    keyCode: 87,
+    keyTrigger: "W",
+    id: "Heater-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    keyCode: 69,
+    keyTrigger: "E",
+    id: "Heater-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    keyCode: 65,
+    keyTrigger: "A",
+    id: "Heater-4",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    keyCode: 83,
+    keyTrigger: "S",
+    id: "Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    keyCode: 68,
+    keyTrigger: "D",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    keyCode: 90,
+    keyTrigger: "Z",
+    id: "Kick-n'-Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    keyCode: 88,
+    keyTrigger: "X",
+    id: "Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    keyCode: 67,
+    keyTrigger: "C",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
 
 const App = () => {
-  const [content, setContent] = useState("");
-  const [parsedContent, setParsedContent] = useState("");
+  const [currentDrum, setCurrentDrum] = useState("");
 
-  marked.setOptions({
-    breaks: true,
-    highlight: function (code: string) {
-      console.log(code);
-      return Prism.highlight(code, Prism.languages.javascript, "javascript");
-    },
-  });
-
-  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    setContent(event.target.value);
-    setParsedContent(marked.parse(event.target.value));
-  };
   useEffect(() => {
     let effect = true;
-    setContent(defaultContent);
-    setParsedContent(marked.parse(defaultContent));
     return () => {
       // eslint-disable-next-line
       effect = false;
@@ -36,54 +74,42 @@ const App = () => {
   return (
     <Box
       className="App"
+      id="drum-machine"
       height="100vh"
       display="flex"
       flexDirection="row"
       alignItems="center"
     >
-      <Box flex={6} height="80%" pr={2} pl={2}>
-        <Box>
-          <Box border="1px solid black">
-            <Typography variant="h6" component="div">
-              Editor
-            </Typography>
-          </Box>
-          <TextareaAutosize
-            className="Editor"
-            id="editor"
-            aria-label="Editor"
-            minRows={34}
-            maxRows={35}
-            value={content}
-            onChange={handleChange}
-            placeholder="Editor"
-          />
-        </Box>
+      <Box id="display" display="none">
+        {currentDrum}
       </Box>
-
-      <Box
-        flex={6}
-        width={"50%"}
-        height="80%"
-        pr={10}
-        pl={10}
-        display="flex"
-        flexDirection="column"
-      >
-        <Box border="1px solid black" flex={1}>
-          <Typography variant="h6" component="div">
-            Preview
-          </Typography>
-        </Box>
-
-        <Box border={1} width={"100%"} height={"100%"} flex={11}>
-          <Typography
-            height={"100%"}
-            id="preview"
-            color="inherit"
-            dangerouslySetInnerHTML={{ __html: parsedContent }}
-          ></Typography>
-        </Box>
+      <Box id="_display" p={10}>
+        {bankOne.map((item) => (
+          <Box key={item.keyTrigger} p={1} m={1}>
+            <Button
+              className="drum-pad"
+              id={`___${item.keyTrigger}`}
+              onClick={() => {
+                setCurrentDrum(item.keyTrigger);
+                const sound = document.getElementById(item.keyTrigger) as any;
+                sound.currentTime = 0;
+                sound.play();
+              }}
+              onKeyDown={(e: any) => {
+                if (e.keyCode === item.keyCode) {
+                  setCurrentDrum(item.keyTrigger);
+                  const sound = document.getElementById(item.keyTrigger) as any;
+                  sound.currentTime = 0;
+                  sound.play();
+                }
+              }}
+              variant="contained"
+            >
+              {item.keyTrigger}
+              <audio className="clip" id={item.keyTrigger} src={item.url} />
+            </Button>
+          </Box>
+        ))}
       </Box>
     </Box>
   );
